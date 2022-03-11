f61307b716041ff094480968c1f3deb89cba5b02
Himanshu Mishra <66140981+himanshu070@users.noreply.github.com>
2022-03-11 23:50:33 +0530

    updated

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,21 @@
-import Head from 'next/head'
-import Image from 'next/image'
+import React, { useState } from 'react'
+import Footer from '../components/Footer'
+import Navbar from '../components/Navbar'
+import Routes from '../components/Routes'
 import styles from '../styles/Home.module.css'
 
+
 export default function Home() {
+  const [darkTheme, setDarkTheme] = useState(true);
+  console.log(darkTheme);
   return (
    <>
-      <div>
-        
+      <div className="">
+         <div className='bg-gray-200 dark:bg-pink-900'>
+            <Navbar/>
+            <Routes/>
+            <Footer/>
+        </div>
       </div>
    </>
   )
