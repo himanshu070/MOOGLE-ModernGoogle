933d1bf88f07f1e81dea9be18ef9072c16ba0240
Himanshu Mishra <66140981+himanshu070@users.noreply.github.com>
2022-03-23 20:51:04 +0530

    Loading added

---
+++ components/Loading.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Loader from "react-loader-spinner"
+
+const Loading = () => {
+  return (
+    <div className=''>
+
+    </div>
+  )
+}
+
+export default Loading
--- components/Results.js
+++ components/Results.js
@@ -1,8 +1,19 @@
 import React from 'react'
+import { useEffect } from 'react'
+import ReactPlayer from 'react-player'
+import { useLocation } from 'react-router-dom'
+
+import ResultContext from '../context/ResultContext'
 
 const Results = () => {
+  const {results, isLoading, getResults, searchTerm } = useResultContext();
+  const location = useLocation();
+  if(isLoading) return <Loading/>;
+
   return (
-    <div>Results</div>
+    <div>
+
+    </div>
   )
 }
 
