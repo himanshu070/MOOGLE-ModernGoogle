6da0aea231e2109727135fd55759a00a67015be2
Himanshu Mishra <66140981+himanshu070@users.noreply.github.com>
2022-03-23 19:49:02 +0530

    Result Context added

--- components/Footer.js
+++ components/Footer.js
@@ -2,10 +2,11 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>
-      <h1>Footer</h1>
+    <div className="text-center p-10 m-10 border-t dark:border-gray-700 border-gray-200">
+      <h1>Made with ❤️ by Himanshu,</h1>
+      <h1>2022, Moogle</h1>
     </div>
-  )
+  );
 }
 
 export default Footer
